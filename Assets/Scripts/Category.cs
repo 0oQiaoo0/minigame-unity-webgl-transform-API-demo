fa6984be1e2e3eadb98cb22245303c6de35d20b0
rainhong <100839497+0oQiaoo0@users.noreply.github.com>
2023-12-13 16:08:03 +0800

    feat: 添加初始滑动页

---
+++ Assets/Scripts/Category.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Category : MonoBehaviour
+{
+    public Text categoryText;
+    public Image categoryImage;
+    
+    public Sprite image;
+    
+    public float unfoldAlpha = 0.5f;
+
+    private bool isUnfold = false;
+
+    private void Start()
+    {
+        categoryText.text = gameObject.name;
+        categoryImage.sprite = image;
+    }
+
+    private Color SetColorWithAlpha(Color color, float alpha)
+    {
+        return new Color(color.r, color.g, color.b, alpha);
+    }
+    
+    private void Unfold()
+    {
+        categoryText.color = SetColorWithAlpha(categoryText.color, unfoldAlpha);
+        categoryImage.color = SetColorWithAlpha(categoryImage.color, unfoldAlpha);
+    }
+    
+    private void Fold()
+    {
+        categoryText.color = SetColorWithAlpha(categoryText.color, 1f);
+        categoryImage.color = SetColorWithAlpha(categoryImage.color, 1f);
+    }
+    
+    public void OnClick()
+    {
+        if (isUnfold)
+        {
+            Fold();
+        }
+        else
+        {
+            Unfold();
+        }
+
+        isUnfold = !isUnfold;
+    }
+}
---
+++ Assets/Scripts/ContentSizeAdjuster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContentSizeAdjuster : MonoBehaviour
+{
+    private RectTransform _content;
+    public RectTransform verticalLayoutGroup;
+
+    private void Awake()
+    {
+        _content = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        UpdateContentSize();
+    }
+
+    private void UpdateContentSize()
+    {
+        Debug.Log(verticalLayoutGroup.rect.height);
+        _content.sizeDelta = new Vector2(_content.sizeDelta.x, verticalLayoutGroup.rect.height);
+    }
+}
